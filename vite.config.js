3d444c28ea4d90ac33ca309c1b26c69bd2baf278
Ryan Balieiro <ryan.balieiro@gmail.com>
2025-06-10 14:27:09 -0300

    First commit.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/react-portfolio-template/',
+    base: '/emily-park-react-portfolio/',
     plugins: [react()],
     build: {
         rollupOptions: {
